9ef685ee30f61232281174ce235331ac56f612bf
Karyn Talyne <karynsilva@gec.inatel.br>
2021-10-05 01:32:23 -0300

    Create CofrinhoInteligente.java

---
+++ Cofre_Maven/src/main/java/cofrinho/CofrinhoInteligente.java
@@ -0,0 +1,16 @@
+package cofrinho;
+
+public class CofrinhoInteligente {
+
+    public static void main(String[] args) {
+        
+        
+        TelaCofrinho tela = new TelaCofrinho();
+        tela.setVisible(true);
+        
+        
+        
+        
+    }
+    
+}
