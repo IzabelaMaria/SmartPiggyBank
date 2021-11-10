cc23d7d71276943940ad13f4a1044a3432fcd9bc
Karyn Talyne <karynsilva@gec.inatel.br>
2021-11-10 06:39:43 -0300

    Create Cofrinho.java

---
+++ Cofre_Maven/src/main/java/cofrinho/Cofrinho.java
@@ -0,0 +1,14 @@
+package cofrinho;
+
+public class Cofrinho {
+
+    public int id;
+    public String nome;
+    public float Cinco_centavos = 0;
+    public float Dez_centavos = 0;
+    public float VinteCinco_centavos = 0;
+    public float Cinquenta_centavos = 0;
+    public float Um_real = 0;
+    public float Valor_Total = 0;
+
+}
