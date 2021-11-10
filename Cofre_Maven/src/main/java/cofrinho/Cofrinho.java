d4630b33ca40906bbc85204634865faf9ff41ed2
Karyn Talyne <karynsilva@gec.inatel.br>
2021-11-10 07:56:39 -0300

    Update Cofrinho.java

--- Cofre_Maven/src/main/java/cofrinho/Cofrinho.java
+++ Cofre_Maven/src/main/java/cofrinho/Cofrinho.java
@@ -1,14 +1,23 @@
 package cofrinho;
 
+import lombok.Data;
+@Data
+
 public class Cofrinho {
 
     public int id;
     public String nome;
-    public float Cinco_centavos = 0;
-    public float Dez_centavos = 0;
-    public float VinteCinco_centavos = 0;
-    public float Cinquenta_centavos = 0;
-    public float Um_real = 0;
-    public float Valor_Total = 0;
+    public int Cinco_centavos = 0;
+    public int Dez_centavos = 0;
+    public int VinteCinco_centavos = 0;
+    public int Cinquenta_centavos = 0;
+    public int Um_real = 0;
+    public int Valor_Total = 0;
+    public String name;
+    
+    public Cofrinho(int user_id,String name){
+        this.id = user_id;
+        this.nome = name;
+    }
 
 }
