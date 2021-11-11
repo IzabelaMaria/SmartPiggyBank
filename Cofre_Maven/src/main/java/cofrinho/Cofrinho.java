7c8d2a0d9065b08509809da3e120c03a99b0d3db
Karyn Talyne <karynsilva@gec.inatel.br>
2021-11-11 05:18:01 -0300

    Update Cofrinho.java

--- Cofre_Maven/src/main/java/cofrinho/Cofrinho.java
+++ Cofre_Maven/src/main/java/cofrinho/Cofrinho.java
@@ -13,11 +13,8 @@ public class Cofrinho {
     public int Cinquenta_centavos = 0;
     public int Um_real = 0;
     public float Valor_Total = 0;
-    public String name;
     
-    public Cofrinho(int user_id,String name){
-        this.id = user_id;
-        this.nome = name;
-    }
+    
+    
 
 }
