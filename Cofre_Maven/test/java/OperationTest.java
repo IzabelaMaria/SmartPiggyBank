f5a45062c2ec3540936f44ce47daf19674a9953f
Marines10 <86080230+Marines10@users.noreply.github.com>
2021-11-16 14:00:46 -0300

    TesteOperaçãocomdefeito

---
+++ Cofre_Maven/test/java/OperationTest.java
@@ -0,0 +1,16 @@
+import Operations;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class OperationTest {
+
+@test 
+
+public void testInserirMoeda() {  
+assertEquals(InserirMoeda(), true);
+}
+
+
+
+}
